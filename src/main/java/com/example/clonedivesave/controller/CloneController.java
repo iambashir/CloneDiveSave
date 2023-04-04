638b1c1edf2d29ad3820ec85bfbdb13c6b8ef016
Dell <74961832+iambashir@users.noreply.github.com>
2023-04-04 15:28:55 +0600

    save data 2

--- src/main/java/com/example/clonedivesave/controller/CloneController.java
+++ src/main/java/com/example/clonedivesave/controller/CloneController.java
@@ -5,6 +5,8 @@ import com.example.clonedivesave.entity.CloneModel;
 import com.example.clonedivesave.service.CloneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +17,16 @@ public class CloneController {
   @Autowired
   CloneService cloneService;
 
-  @RequestMapping("/save-clone")
+  @GetMapping("/all-data")
+  public String allData(Model model){
+    return "Home.html";
+  }
+
+  @PostMapping("/save-clone")
   public String SaveAllClone(@ModelAttribute("clone") CloneModel clone){
     cloneService.saveAllClone(clone);
-    return "Home.html";
+    System.out.println(clone.getTestName());
+    return "redirect:/all-data";
   }
 
 
